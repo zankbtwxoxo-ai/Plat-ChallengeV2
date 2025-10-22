db91d078fd53d4da97e94c627f20019688eb701a
zankbtwxoxo-ai <brandenbourglionel@gmail.com>
2025-10-22 15:42:41 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,26 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Shell from './shell'
 import Concept from './pages/Concept'
-import Inscription from './pages/Inscription'
 import Classement from './pages/Classement'
+import Inscription from './pages/Inscription'
+import './index.css'
 
-const RootApp: React.FC = () => (
-  <BrowserRouter>
-    <Shell>
-      <Routes>
-        <Route path='/' element={<Concept />} />
-        <Route path='/inscription' element={<Inscription />} />
-        <Route path='/classement' element={<Classement />} />
-      </Routes>
-    </Shell>
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Shell>
+        <Routes>
+          {/* Redirection de la page d’accueil vers /classement */}
+          <Route path="/" element={<Navigate to="/classement" replace />} />
+          <Route path="/concept" element={<Concept />} />
+          <Route path="/classement" element={<Classement />} />
+          <Route path="/inscription" element={<Inscription />} />
+          {/* Si l’utilisateur entre une mauvaise URL, on le renvoie sur /classement */}
+          <Route path="*" element={<Navigate to="/classement" replace />} />
+        </Routes>
+      </Shell>
+    </BrowserRouter>
+  </React.StrictMode>
 )
-
-createRoot(document.getElementById('root')!).render(<RootApp />)
